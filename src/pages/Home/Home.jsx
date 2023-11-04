7dfce6cdeb527876fd8d57256a02c100d935538f
窪山天望 <shiggyjr1492@gmail.com>
2023-11-04 14:22:33 +0900

    fix: Linkをaタグに修正しました。

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,65 +17,65 @@ const Home = () => {
                     <div className="col-sm-4">
                         <div className="card">
                             <div className="card-body">
-                                <Link to="/linergradient">
+                                <a href="/linergradient">
                                     <h3 className="card-title">liner-gradient</h3>
                                     <div className="card-icon text-center">
                                         <img src="/images/liner-gradient-icon.png" alt="" />
                                     </div>
                                     <p className="card-text"></p>
-                                </Link>
+                                </a>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-4">
                         <div className="card">
                             <div className="card-body">
-                                <Link to="/boxshadow">
+                                <a href="/boxshadow">
                                     <h3 className="card-title">box-shadow</h3>
                                     <div className="card-icon text-center">
                                         <img src="/images/Box-Shadow-icon.png" alt="" />
                                     </div>
                                     <p className="card-text"></p>
-                                </Link>
+                                </a>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-4">
                         <div className="card">
                             <div className="card-body">
-                                <Link to="/textshadow">
+                                <a href="/textshadow">
                                     <h3 className="card-title">text-shadow</h3>
                                     <div className="card-icon text-center">
                                         <img src="/images/text-shadow-icon.png" alt="" />
                                     </div>
                                     <p className="card-text"></p>
-                                </Link>
+                                </a>
                             </div>
                         </div>
                     </div>
                     <div className='col-sm-4'>
                         <div className="card">
                             <div className="card-body">
-                                <Link to="/imgfilter">
+                                <a href="/imgfilter">
                                     <h3 className="card-title">image-filter-generater</h3>
                                     <div className="card-icon text-center">
                                         <img src="/images/image-filter-generater-icon.png" alt="" />
                                     </div>
                                     <p className="card-text"></p>
-                                </Link>
+                                </a>
                             </div>
                         </div>
                     </div>
                     <div className="col-sm-4">
                         <div className="card">
                             <div className="card-body">
-                                <Link to="/qrcode">
+                                <a href="/qrcode">
                                     <h3 className="card-title">QRcode-generater</h3>
                                     <div className="card-icon text-center">
                                         <img src="/images/qrcode-generater-icon.png" alt="" />
                                     </div>
                                     <p className="card-text"></p>
-                                </Link>
+                                </a>
                             </div>
                         </div>
                     </div>
