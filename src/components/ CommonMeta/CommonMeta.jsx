6eb91dd64766c43e986f859bb863a38b67d82e4c
窪山天望 <shiggyjr1492@gmail.com>
2023-11-04 17:09:01 +0900

    fix: react-helmetをreact-helmet-asyncに変更しました。

--- src/components/ CommonMeta/CommonMeta.jsx
+++ src/components/ CommonMeta/CommonMeta.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
+import { Helmet } from 'react-helmet-async'
 
 const CommonMeta = ({ title, imgUrl, type, description, url }) => {
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { AuthProvider } from './Context/AuthContext.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import './index.css'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
     <React.StrictMode>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </React.StrictMode>
   </AuthProvider>
 )
